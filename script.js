e057f0648ed89c503cda8c3531dddbeb4db93a40
ayoub-akraou <87671584+ayoub-akraou@users.noreply.github.com>
2022-05-05 08:39:31 +0000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector(".btn");
-const root = document.documentElement;
-btn.addEventListener("click", function (e) {
-  e.preventDefault();
-  document.body.classList.toggle("dark-mode");
-});
